c6e1dd797a605a5f8ec173a7ab7063a5a0492964
Vadim Godunko <godunko@adacore.com>
2017-09-23 13:29:26 +0200

    Switch to use Universal_String.

--- sources/generator/ada_side-driver.adb
+++ sources/generator/ada_side-driver.adb
@@ -1,6 +1,8 @@
-with Ada.Command_Line;
-with Ada.Text_IO;
+with Ada.Wide_Wide_Text_IO;
 with Interfaces.C;
+
+with League.Application;
+with League.String_Vectors;
 
 with Abstract_Meta_Class_Lists;
 with Abstract_Meta_Classes;
@@ -11,6 +13,8 @@ with Q_Strings;
 with Ada_Side.Setup;
 
 procedure Ada_Side.Driver is
+   Arguments  : constant League.String_Vectors.Universal_String_Vector
+     := League.Application.Arguments;
    Extractor  : API_Extractors.API_Extractor;
    Classes    : Abstract_Meta_Class_Lists.Abstract_Meta_Class_List;
    Generators : constant Ada_Side.Setup.Generator_Array
@@ -18,15 +22,15 @@ procedure Ada_Side.Driver is
 
 begin
    Extractor.Set_Cpp_File_Name
-     (Q_Strings.From_UTF8 (Ada.Command_Line.Argument (1)));
+     (Q_Strings.From_Universal_String (Arguments (1)));
 
    Extractor.Set_Type_System
-     (Q_Strings.From_UTF8 (Ada.Command_Line.Argument (2)));
+     (Q_Strings.From_Universal_String (Arguments (2)));
 
-   for J in 3 .. Ada.Command_Line.Argument_Count loop
+   for J in 3 .. Arguments.Length loop
       Extractor.Add_Include_Path
-        (Header_Paths.Create
-           (Q_Strings.From_UTF8 (Ada.Command_Line.Argument (J))));
+       (Header_Paths.Create
+         (Q_Strings.From_Universal_String (Arguments (J))));
    end loop;
 
    if Extractor.Run then
@@ -35,15 +39,18 @@ begin
 
    Classes := Extractor.Classes;
 
-   Ada.Text_IO.Put_Line (Interfaces.C.int'Image (Classes.Size));
+   Ada.Wide_Wide_Text_IO.Put_Line
+    (Interfaces.C.int'Wide_Wide_Image (Classes.Size));
 
    for Class of Classes loop
-      Ada.Text_IO.Put_Line
-       (" " & Class.Full_Name.To_UTF8 & " " & Class.Type_Entry.Name.To_UTF8);
+      Ada.Wide_Wide_Text_IO.Put_Line
+       (" " & Class.Full_Name.To_Universal_String.To_Wide_Wide_String
+          & " "
+          & Class.Type_Entry.Name.To_Universal_String.To_Wide_Wide_String);
 
       for Generator of Generators loop
          if Generator.Should_Generate (Class) then
-            Ada.Text_IO.Put_Line ("   ...generating...");
+            Ada.Wide_Wide_Text_IO.Put_Line ("   ...generating...");
             Generator.Generate (Class);
          end if;
       end loop;
