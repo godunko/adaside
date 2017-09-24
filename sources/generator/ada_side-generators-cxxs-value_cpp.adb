7976dd85ae552a4a99338d4427df4205fd9a2df5
Vadim Godunko <godunko@adacore.com>
2017-09-24 14:02:08 +0200

    Generate C++ code for value types.

--- sources/generator/ada_side-generators-cxxs-value_cpp.adb
+++ sources/generator/ada_side-generators-cxxs-value_cpp.adb
@@ -2,6 +2,10 @@ with Ada_Side.Units;
 
 package body Ada_Side.Generators.CXXs.Value_Cpp is
 
+   function "+"
+    (Item : Wide_Wide_String) return League.Strings.Universal_String
+       renames League.Strings.To_Universal_String;
+
    --------------
    -- Generate --
    --------------
@@ -10,10 +14,57 @@ package body Ada_Side.Generators.CXXs.Value_Cpp is
     (Self  : in out Value_Cpp_Generator;
      Class : Abstract_Meta_Classes.Abstract_Meta_Class'Class)
    is
+      use type League.Strings.Universal_String;
+
       Unit : Ada_Side.Units.CXX_Cpp_Unit;
 
    begin
       Unit.Set_Class_Name (Class.Name.To_Universal_String);
+
+      Unit.Put_Line
+       ("#include """
+          & Class.Name.To_Universal_String.To_Lowercase & "_wrapper.h""");
+
+      Unit.New_Line;
+      Unit.Put_Line
+       ("const size_t "
+          & API_Size_Of_Link_Name (Class)
+          & " = sizeof(" & Class.Name.To_Universal_String & ");");
+
+      Unit.New_Line;
+      Unit.Put_Line
+       ("void "
+          & API_Subprogram_Link_Name (Class, Adjust)
+          & "(" & Class.Name.To_Universal_String
+          & "** ___view, void* ___storage)");
+      Unit.Put_Line (+"{");
+      Unit.Put_Line
+       (+"    *___view = new (___storage) "
+           & Class.Name.To_Universal_String & "(**___view);");
+      Unit.Put_Line (+"}");
+
+      Unit.New_Line;
+      Unit.Put_Line
+       ("void "
+          & API_Subprogram_Link_Name (Class, Finalize)
+          & "(" & Class.Name.To_Universal_String & "** ___view)");
+      Unit.Put_Line (+"{");
+      Unit.Put_Line
+       ("    (*___view)->~" & Class.Name.To_Universal_String & "();");
+      Unit.Put_Line (+"    *___view = 0;");
+      Unit.Put_Line (+"}");
+
+      Unit.New_Line;
+      Unit.Put_Line
+       ("void "
+          & API_Subprogram_Link_Name (Class, Initialize)
+          & "(" & Class.Name.To_Universal_String
+          & "** ___view, void* ___storage)");
+      Unit.Put_Line (+"{");
+      Unit.Put_Line
+       (+"    *___view = new (___storage) "
+           & Class.Name.To_Universal_String & "();");
+      Unit.Put_Line (+"}");
 
       Unit.Save (Self.Output_Directory);
    end Generate;
--- sources/generator/ada_side-generators-cxxs-value_h.adb
+++ sources/generator/ada_side-generators-cxxs-value_h.adb
@@ -10,10 +10,34 @@ package body Ada_Side.Generators.CXXs.Value_H is
     (Self  : in out Value_H_Generator;
      Class : Abstract_Meta_Classes.Abstract_Meta_Class'Class)
    is
+      use type League.Strings.Universal_String;
+
       Unit : Ada_Side.Units.CXX_H_Unit;
 
    begin
       Unit.Set_Class_Name (Class.Name.To_Universal_String);
+
+      Unit.Put_Line ("#include <" & Class.Name.To_Universal_String & ">");
+
+      Unit.New_Line;
+      Unit.Put_Line
+       ("extern ""C"" const size_t " & API_Size_Of_Link_Name (Class) & ";");
+
+      Unit.New_Line;
+      Unit.Put_Line
+       ("extern ""C"" void "
+          & API_Subprogram_Link_Name (Class, Adjust)
+          & "(" & Class.Name.To_Universal_String
+          & "** ___view, void* ___storage);");
+      Unit.Put_Line
+       ("extern ""C"" void "
+          & API_Subprogram_Link_Name (Class, Finalize)
+          & "(" & Class.Name.To_Universal_String & "** ___view);");
+      Unit.Put_Line
+       ("extern ""C"" void "
+          & API_Subprogram_Link_Name (Class, Initialize)
+          & "(" & Class.Name.To_Universal_String
+          & "** ___view, void* ___storage);");
 
       Unit.Save (Self.Output_Directory);
    end Generate;
