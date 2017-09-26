ed18f911dcf81d8bdc36f888a77cff03e4d77cde
Vadim Godunko <godunko@adacore.com>
2017-09-26 09:49:14 +0200

    Generate user spec for function which return value type.

--- sources/generator/ada_side-generators-adas-value_spec.adb
+++ sources/generator/ada_side-generators-adas-value_spec.adb
@@ -1,5 +1,11 @@
+with Ada.Wide_Wide_Text_IO;
+with Interfaces.C;
+
 with League.Characters;
 with League.Strings;
+
+with Abstract_Meta_Function_Lists;
+with Abstract_Meta_Types;
 
 with Ada_Side.Units;
 
@@ -23,6 +29,18 @@ package body Ada_Side.Generators.Adas.Value_Spec is
    Q_Underline_Prefix : constant League.Strings.Universal_String
      := League.Strings.To_Universal_String ("Q_");
 
+   ----------------
+   -- Find_Class --
+   ----------------
+
+   function Find_Class
+    (Self       : Value_Ada_Spec_Generator;
+     Type_Entry : Type_Entries.Type_Entry)
+       return Abstract_Meta_Classes.Abstract_Meta_Class is
+   begin
+      return Abstract_Meta_Classes.Find_Class (Self.Classes, Type_Entry);
+   end Find_Class;
+
    --------------
    -- Generate --
    --------------
@@ -31,7 +49,10 @@ package body Ada_Side.Generators.Adas.Value_Spec is
     (Self  : in out Value_Ada_Spec_Generator;
      Class : Abstract_Meta_Classes.Abstract_Meta_Class'Class)
    is
-      Unit : Ada_Side.Units.Ada_Spec_Unit;
+      use type Abstract_Meta_Classes.Abstract_Meta_Class;
+
+      Unit      : Ada_Side.Units.Ada_Spec_Unit;
+      Functions : Abstract_Meta_Function_Lists.Abstract_Meta_Function_List;
 
    begin
       Unit.Set_Package_Name (User_Package_Full_Name (Class));
@@ -46,6 +67,56 @@ package body Ada_Side.Generators.Adas.Value_Spec is
       Unit.New_Line;
       Unit.Put_Line
        ("   type " & User_Tagged_Type_Name (Class) & " is tagged private;");
+
+      Functions := Class.Functions;
+
+      for Method of Functions loop
+         declare
+            use type Interfaces.C.int;
+
+            Return_Type  : constant Abstract_Meta_Types.Abstract_Meta_Type
+              := Method.Get_Type;
+            Return_Class : constant Abstract_Meta_Classes.Abstract_Meta_Class
+              := (if Return_Type.Is_Null
+                    then Abstract_Meta_Classes.Null_Abstract_Meta_Class
+                    else Self.Find_Class (Return_Type.Type_Entry));
+
+         begin
+            if Method.Is_Constructor then
+               --  XXX Not supported yet.
+
+               Ada.Wide_Wide_Text_IO.Put_Line
+                ("Skipping "
+                   & Method.Name.To_Universal_String.To_Wide_Wide_String);
+
+            elsif Abstract_Meta_Classes.Abstract_Meta_Class (Class)
+                    = Return_Class
+              and Method.Is_Constant
+              and Method.Arguments.Size = 0
+              and Method.Get_Type.Is_Value
+              and not Method.Get_Type.Is_Reference
+            then
+               Unit.New_Line;
+               Unit.Put_Line
+                ("   function "
+                   & To_Ada_Identifier (Method.Name.To_Universal_String));
+               Unit.Put_Line
+                ("    (Self : "
+                   & User_Tagged_Type_Name (Class) & "'Class)");
+               Unit.Put_Line
+                ("       return "
+                   & User_Tagged_Type_Name (Return_Class) & ";");
+
+            else
+               --  XXX Not supported yet.
+
+               Ada.Wide_Wide_Text_IO.Put_Line
+                ("Skipping "
+                   & Method.Name.To_Universal_String.To_Wide_Wide_String);
+            end if;
+         end;
+      end loop;
+
       Unit.New_Line;
       Unit.Put_Line (+"private");
       Unit.New_Line;
@@ -129,7 +200,10 @@ package body Ada_Side.Generators.Adas.Value_Spec is
    begin
       return Result : League.Strings.Universal_String do
          for J in 1 .. Name.Length loop
-            if J > 1 then
+            if J = 1 then
+               Result.Append (Name (J).Simple_Uppercase_Mapping);
+
+            else
                if J = 2
                  and Name (J - 1) = 'Q'
                  and Name (J).Uppercase
@@ -143,9 +217,9 @@ package body Ada_Side.Generators.Adas.Value_Spec is
                then
                   Result.Append ('_');
                end if;
-            end if;
 
-            Result.Append (Name (J));
+               Result.Append (Name (J));
+            end if;
          end loop;
       end return;
    end To_Ada_Identifier;
