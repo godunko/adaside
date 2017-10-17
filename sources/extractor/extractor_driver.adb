025cc096f65fd0852ad3f8e669a499f100dd9a4e
Vadim Godunko <godunko@adacore.com>
2017-10-17 03:48:59 +0200

    Reuse precompiled AST tree when available.

--- sources/extractor/extractor_driver.adb
+++ sources/extractor/extractor_driver.adb
@@ -1,3 +1,4 @@
+with Ada.Directories;
 with Interfaces.C.Strings;
 
 with League.Application;
@@ -6,6 +7,7 @@ with League.Strings;
 with clang_c_CXErrorCode_h;
 with clang_c_Index_h;
 
+with Clang.Translation_Units;
 with Clang_Utilities;
 with Code_Model.Builders;
 
@@ -14,31 +16,49 @@ procedure Extractor_Driver is
    use type Interfaces.C.unsigned;
    use type clang_c_CXErrorCode_h.CXErrorCode;
 
-   Source_File : Interfaces.C.Strings.chars_ptr
+   Source_File      : Interfaces.C.Strings.chars_ptr
      := Interfaces.C.Strings.New_String ("global.h");
+   Precompiled_File : constant League.Strings.Universal_String
+     := League.Strings.To_Universal_String ("global.pch");
 
    Index   : clang_c_Index_h.CXIndex;
    Error   : clang_c_CXErrorCode_h.CXErrorCode;
    Unit    : clang_c_Index_h.CXTranslationUnit;
    Cursor  : clang_c_Index_h.CXCursor;
    Dummy   : Interfaces.C.unsigned;
+   Dummy_2 : clang_c_Index_h.CXSaveError;
    Builder : Code_Model.Builders.Code_Model_Builder;
 
 begin
    Index := clang_c_Index_h.clang_createIndex (0, 1);
-   Error :=
-     Clang_Utilities.Parse_Translation_Unit
-      (Index,
-       League.Strings.To_Universal_String ("global.h"),
-       League.Application.Arguments,
-       clang_c_Index_h.CXTranslationUnit_Incomplete
-         and clang_c_Index_h.CXTranslationUnit_SkipFunctionBodies,
-       Unit);
 
-   if Error /= clang_c_CXErrorCode_h.CXError_Success then
-      raise Program_Error
-        with "parseTranslationUnit returns "
-               & clang_c_CXErrorCode_h.CXErrorCode'Image (Error);
+   if not Ada.Directories.Exists (Precompiled_File.To_UTF_8_String) then
+      Error :=
+        Clang_Utilities.Parse_Translation_Unit
+         (Index,
+          League.Strings.To_Universal_String ("global.h"),
+          League.Application.Arguments,
+          clang_c_Index_h.CXTranslationUnit_Incomplete
+            and clang_c_Index_h.CXTranslationUnit_SkipFunctionBodies,
+          Unit);
+
+      if Error = clang_c_CXErrorCode_h.CXError_Success then
+         Dummy_2 := Clang.Translation_Units.Save (Unit, Precompiled_File);
+
+      else
+         raise Program_Error
+           with "parseTranslationUnit returns "
+                  & clang_c_CXErrorCode_h.CXErrorCode'Image (Error);
+      end if;
+
+   else
+      Error := Clang.Translation_Units.Create (Index, Precompiled_File, Unit);
+
+      if Error /= clang_c_CXErrorCode_h.CXError_Success then
+         raise Program_Error
+           with "parseTranslationUnit returns "
+                  & clang_c_CXErrorCode_h.CXErrorCode'Image (Error);
+      end if;
    end if;
 
    Cursor := clang_c_Index_h.clang_getTranslationUnitCursor (Unit);
