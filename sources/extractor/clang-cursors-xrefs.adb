c2ece89bc656d00e037acf9ffcf522e8982a89b0
Vadim Godunko <godunko@adacore.com>
2017-10-17 02:56:07 +0200

    Convinient wrapper for clang_isCursorDefinition().

--- sources/extractor/clang-cursors-xrefs.adb
+++ sources/extractor/clang-cursors-xrefs.adb
@@ -1,3 +1,4 @@
+with Interfaces.C;
 
 package body Clang.Cursors.Xrefs is
 
@@ -12,5 +13,16 @@ package body Clang.Cursors.Xrefs is
       return
         To_Universal_String (clang_c_Index_h.clang_getCursorSpelling (Cursor));
    end Get_Spelling;
+
+   -------------------
+   -- Is_Definition --
+   -------------------
+
+   function Is_Definition (Cursor : clang_c_Index_h.CXCursor) return Boolean is
+      use type Interfaces.C.unsigned;
+
+   begin
+      return clang_c_Index_h.clang_isCursorDefinition (Cursor) /= 0;
+   end Is_Definition;
 
 end Clang.Cursors.Xrefs;
--- sources/extractor/clang-cursors-xrefs.ads
+++ sources/extractor/clang-cursors-xrefs.ads
@@ -6,5 +6,9 @@ package Clang.Cursors.Xrefs is
 
    function Get_Spelling
     (Cursor : clang_c_Index_h.CXCursor) return League.Strings.Universal_String;
+   --  Wrapper for clang_getCursorSpelling()
+
+   function Is_Definition (Cursor : clang_c_Index_h.CXCursor) return Boolean;
+   --  Wrapper for clang_isCursorDefinition()
 
 end Clang.Cursors.Xrefs;
