8a99372b495fbe8e4247d89945edb140f6059d68
Vadim Godunko <godunko@adacore.com>
2017-10-17 03:48:36 +0200

    Wrapper for clang_createTranslationUnit2().

--- sources/extractor/clang-translation_units.adb
+++ sources/extractor/clang-translation_units.adb
@@ -2,27 +2,53 @@ with Interfaces.C.Strings;
 
 package body Clang.Translation_Units is
 
+   ------------
+   -- Create --
+   ------------
+
+   function Create
+    (Index            : clang_c_Index_h.CXIndex;
+     AST_File_Name    : League.Strings.Universal_String;
+     Translation_Unit : out clang_c_Index_h.CXTranslationUnit)
+       return clang_c_CXErrorCode_h.CXErrorCode
+   is
+      C_AST_File_Name : Interfaces.C.Strings.chars_ptr
+        := Interfaces.C.Strings.New_String (AST_File_Name.To_UTF_8_String);
+      Result          : clang_c_CXErrorCode_h.CXErrorCode;
+
+   begin
+      Result :=
+        clang_c_Index_h.clang_createTranslationUnit2
+         (Index,
+          C_AST_File_Name,
+          Translation_Unit'Address);
+
+      Interfaces.C.Strings.Free (C_AST_File_Name);
+
+      return Result;
+   end Create;
+
    ----------
    -- Save --
    ----------
 
    function Save
-    (Unit      : clang_c_Index_h.CXTranslationUnit;
-     File_Name : League.Strings.Universal_String)
+    (Unit          : clang_c_Index_h.CXTranslationUnit;
+     AST_File_Name : League.Strings.Universal_String)
        return clang_c_Index_h.CXSaveError
    is
-      C_File_Name : Interfaces.C.Strings.chars_ptr
-        := Interfaces.C.Strings.New_String (File_Name.To_UTF_8_String);
+      C_AST_File_Name : Interfaces.C.Strings.chars_ptr
+        := Interfaces.C.Strings.New_String (AST_File_Name.To_UTF_8_String);
 
    begin
       return Result : constant clang_c_Index_h.CXSaveError
         := clang_c_Index_h.CXSaveError'Val
             (clang_c_Index_h.clang_saveTranslationUnit
               (Unit,
-               C_File_Name,
+               C_AST_File_Name,
                clang_c_Index_h.clang_defaultSaveOptions (Unit)))
       do
-         Interfaces.C.Strings.Free (C_File_Name);
+         Interfaces.C.Strings.Free (C_AST_File_Name);
       end return;
    end Save;
 
--- sources/extractor/clang-translation_units.ads
+++ sources/extractor/clang-translation_units.ads
@@ -1,14 +1,22 @@
 with League.Strings;
 
+with clang_c_CXErrorCode_h;
 with clang_c_Index_h;
 
 package Clang.Translation_Units is
 
    function Save
-    (Unit      : clang_c_Index_h.CXTranslationUnit;
-     File_Name : League.Strings.Universal_String)
+    (Unit          : clang_c_Index_h.CXTranslationUnit;
+     AST_File_Name : League.Strings.Universal_String)
        return clang_c_Index_h.CXSaveError;
-   --  wrapper of clang_saveTranslationUnit(), clang_defaultSaveOptions() is
+   --  Wrapper of clang_saveTranslationUnit(), clang_defaultSaveOptions() is
    --  used to obtain save options.
+
+   function Create
+    (Index            : clang_c_Index_h.CXIndex;
+     AST_File_Name    : League.Strings.Universal_String;
+     Translation_Unit : out clang_c_Index_h.CXTranslationUnit)
+       return clang_c_CXErrorCode_h.CXErrorCode;
+   --  Wrapper for clang_createTranslationUnit2().
 
 end Clang.Translation_Units;
